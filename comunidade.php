e1e230c32f7f28206f1c170d6ffd64a10882ae68
AnaJuliaPin1h <anajuliapinheiro517@gmail.com>
2026-03-17 15:59:26 -0300

    .

---
+++ comunidade.php
@@ -0,0 +1,22 @@
+<?php
+    include('protect.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comunidade | Pet-code</title>
+    <link rel="icon" href="Pet-Code.png" type="image/png">
+    <link rel="stylesheet" href="Css/comunidade.css">
+</head>
+<body>
+    <p>
+        <a href="login.php"> Sair </a>
+    </p>
+    
+</body>
+</html>
